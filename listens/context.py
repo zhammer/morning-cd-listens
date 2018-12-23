b82a1d37f7fba40b281596a516e23f484962002b
zhammer <zachary_hammer@alumni.brown.edu>
2018-12-22 20:13:11 -0500

    Update use case

--- listens/context.py
+++ listens/context.py
@@ -2,10 +2,12 @@ from typing import NamedTuple
 
 from listens.gateways.db import DbGatewayABC
 from listens.gateways.music import MusicGatewayABC
+from listens.gateways.notification_gateway_abc import NotificationGateway as NotificationGatewayABC
 from listens.gateways.sunlight import SunlightGatewayABC
 
 
 class Context(NamedTuple):
     db_gateway: DbGatewayABC
     music_gateway: MusicGatewayABC
+    notification_gateway: NotificationGatewayABC
     sunlight_gateway: SunlightGatewayABC
